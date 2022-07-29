409c858ff6b5e6dd9981b473825ed2a31ff7c63f
user name <user emaile>
2022-07-30 00:58:38 +0300

    classLab04

--- src/component/main.js
+++ src/component/main.js
@@ -2,24 +2,46 @@ import React from "react";
 import HornedBeast from "./hornedbeast";
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+
+
 class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             dataBeast: this.props.data
         }
+ 
     }
+    filterData = (e) => {
+         
+        this.props.filterBeastByHornNumber(e);
+     }
+     
     render() {
         return (
-            <Row xs={1} md={3} lg={4} className="g-4">
-                {
-                    this.state.dataBeast.map(animal =>
-                        <Col>
-                            <HornedBeast dataBeast={animal} displayModal={this.props.displayModal} />
-                        </Col>
-                    )
-                }
-            </Row>
+
+            <>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor="disabledSelect">Filter Beast By Horn Number</Form.Label>
+                    <Form.Select   onChange={this.filterData}>
+                        <option value='' >All</option>
+                        <option value='1' >One</option>
+                        <option value='2' >Two</option>
+                        <option value='3' >Three</option>
+                        <option value='100' >WooW!</option>
+                    </Form.Select>
+                </Form.Group>
+                <Row xs={1} md={3} lg={4} className="g-4">
+                    {
+                        this.state.dataBeast.map(animal =>
+                            <Col>
+                                <HornedBeast dataBeast={animal} displayModal={this.props.displayModal} />
+                            </Col>
+                        )
+                    }
+                </Row>
+            </>
         )
     }
 }
